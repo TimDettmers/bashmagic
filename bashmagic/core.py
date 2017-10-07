6aaab871a01036ae1e4982629595960216d04bd1
Tim Dettmers <tim.dettmers@gmail.com>
2017-10-07 16:16:05 +0100

    Added wmctrl functionality.

--- bashmagic/core.py
+++ bashmagic/core.py
@@ -1,5 +1,6 @@
 import subprocess
 import operator
+import re
 
 from os import listdir
 from os.path import isfile, join, isdir, splitext
@@ -27,8 +28,19 @@ def unzip(input_path, output_path=None):
     return execute(strCMD)
 
 def execute(strCMD):
-    return subprocess.call(strCMD, shell=True)
+    try:
+        return subprocess.check_output(strCMD, shell=True, universal_newlines=True)
+    except:
+        return None
 
+def execute_and_return(strCMD):
+    proc = subprocess.Popen(shlex.split(strCMD), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    out, err = out.decode("UTF-8").strip(), err.decode("UTF-8").strip()
+    if err != '':
+        return err
+    else:
+        return out
 
 def get_files_by_filetype(path):
     filetype2paths = {}
@@ -40,3 +52,28 @@ def get_files_by_filetype(path):
             if ext not in filetype2paths: filetype2paths[ext] = []
             filetype2paths[ext].append(f)
     return filetype2paths
+
+
+def get_active_window_coordinates():
+    data = execute('xdotool getactivewindow getwindowgeometry')
+    data = data.split('\n')
+    data = data[2][12:]
+    x, y = data.split('x')
+    return int(x),int(y)
+
+
+def get_active_window_name():
+    return execute('xdotool getactivewindow getwindowname')
+
+def get_active_window_path():
+    pid = execute('xdotool getactivewindow getwindowpid').strip()
+    cmd = 'ps -ef | grep {0}'.format(pid)
+    results = execute(cmd).split('\n')
+    for r in results:
+        r = re.sub('\s+', ' ', r)
+        values = r.split(' ')
+        if len(values) < 2: continue
+        if values[1] == pid:
+            path = values[7]
+    return path
+
